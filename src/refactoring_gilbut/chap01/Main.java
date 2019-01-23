050d6f801b8981155414b0b76a36b49f7c005f57
mia94 <mia_3@hanmail.net>
2019-01-23 17:28:18 +0900

    main 수정

--- src/refactoring_gilbut/chap01/Main.java
+++ src/refactoring_gilbut/chap01/Main.java
@@ -2,6 +2,8 @@ package refactoring_gilbut.chap01;
 
 import refactoring_gilbut.chap01.after.RobotSymbolicConstant;
 import refactoring_gilbut.chap01.after2.RobotCommandClass;
+import refactoring_gilbut.chap01.after3.Command;
+import refactoring_gilbut.chap01.after3.RobotEnum;
 import refactoring_gilbut.chap01.before.RobotMagicNumber;
 
 public class Main {
@@ -9,17 +11,30 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		before();
-		after();
-		System.out.println("after2Main()");
+		afterSysbolicConstant();
+		afterClass();
+		afterEnum();
+
+	}
+
+	private static void afterEnum() {
+		System.out.println("afterEnum()");
+		RobotEnum robot = new RobotEnum("김보민");
+		robot.order(Command.WORK);
+		robot.order(Command.STOP);
+		robot.order(Command.JUMP);
+	}
+
+	private static void afterClass() {
+		System.out.println("afterClass()");
 		RobotCommandClass robot = new RobotCommandClass("Andrew");
 		robot.order(RobotCommandClass.COMMAND_WALK);//walk
 		robot.order(RobotCommandClass.COMMAND_STOP);//stop
 		robot.order(RobotCommandClass.COMMAND_JUMP);//jump 
-
 	}
 
-	private static void after() {
-		System.out.println("afterMain()");
+	private static void afterSysbolicConstant() {
+		System.out.println("afterSysbolicConstant()");
 		RobotSymbolicConstant robot = new RobotSymbolicConstant("Andrew");
 		robot.order(RobotSymbolicConstant.COMMAND_WALK);//walk
 		robot.order(RobotSymbolicConstant.COMMAND_STOP);//stop
